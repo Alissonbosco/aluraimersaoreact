5ab48ee31189a42d683842ed47f9adc411c37159
Alissonbosco <abosco874@gmail.com>
2022-01-26 14:54:00 -0300

    adicionando pagina do chat

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a2b82a40b10991d4"; }
+/******/ 		__webpack_require__.h = function() { return "de4ae4c96aea33eb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
